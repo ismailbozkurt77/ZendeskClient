a6c04e67224bd8a3933c2283b3baa410ac7aa520
Ismail Bozkurt <mr.grywolf@gmail.com>
2017-01-07 19:44:58 +0000

    Ticket List Module builder initialized

---
+++ ZendeskExercise/Modules/TicketList/TicketListModuleBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  TicketListModuleBuilder.swift
+//  ZendeskExercise
+//
+//  Created by Ismail on 07/01/2017.
+//  Copyright © 2017 Zendesk. All rights reserved.
+//
+
+import UIKit
+
+class TicketListModuleBuilder: NSObject {
+
+}
