ff1e2f51559096cb991cfada1a843a293bcc262e
Ismail Bozkurt <mr.grywolf@gmail.com>
2017-01-08 10:35:59 +0000

    Ticket List Builder completed.

--- ZendeskExercise/Modules/TicketList/TicketListModuleBuilder.swift
+++ ZendeskExercise/Modules/TicketList/TicketListModuleBuilder.swift
@@ -7,7 +7,25 @@
 //
 
 import UIKit
+import ZendeskNetworking
+
+private let kTicketListModuleStoryboardName = "TicketList"
+
 
 class TicketListModuleBuilder: NSObject {
-
+    func buildTicketlistModule() -> UIViewController{
+        let restClient: RestClient = UrlSessionRestClient()
+        let service = TicketDefaultService(restClient: restClient)
+        let interactor: TicketListInteractor = TicketListDefaultInteractor(service: service)
+        
+        let storyboard = UIStoryboard(name: kTicketListModuleStoryboardName,
+                                      bundle: Bundle.main)
+        let viewControllerName = String(describing: TicketListTableViewController.self)
+        let ticketListViewController = storyboard.instantiateViewController(withIdentifier: viewControllerName) as! TicketListTableViewController
+        
+        let eventHandler: TicketListEventHandler = TicketListPresenter(view: ticketListViewController, interactor: interactor)
+        ticketListViewController.eventHandler = eventHandler        
+        
+        return ticketListViewController
+    }
 }
