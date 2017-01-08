1007b50c78522f3652aa46d663f0916cecd691ef
Ismail Bozkurt <mr.grywolf@gmail.com>
2017-01-08 13:01:29 +0000

    + Localization support for English

---
+++ ZendeskExercise/Resources/Localization/Localization.swift
@@ -0,0 +1,9 @@
+//
+//  Localization.swift
+//  ZendeskExercise
+//
+//  Created by Ismail on 08/01/2017.
+//  Copyright © 2017 Zendesk. All rights reserved.
+//
+
+public let kCommonStringTable = "Common"
